1f3edae0c1b159035c4d55fb74a97a38620bdaf4
Kristiana Blake <kristiana.blake@gmail.com>
2020-02-16 20:57:35 -0600

    removed more psuedo code

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-// arrange the letters in increasing frequency 
-// 	starting with 1 and continuing without gaps and without duplicates
-
-
-// make sure that only pyramid words return true and 
-// non-pyramid words return false 
-
-// if freq equals increasing array then return true 
-// if freq does not equal increasing array then return false
-
 
 const check = {
 
@@ -61,8 +51,6 @@ const check = {
 
 $('form').on('submit', (event) => {
 	event.preventDefault();
-
-
 
 	// this reminds me that I clicked on the button
 	const inputBox = $('#input-box');
