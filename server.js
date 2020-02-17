cf8caeac1c1a28f90e355a0b8077d219e3b43393
Kristiana Blake <kristiana.blake@gmail.com>
2020-02-17 13:06:53 -0600

    express is connected to index.html file

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, '/js/app.js')));
+
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
