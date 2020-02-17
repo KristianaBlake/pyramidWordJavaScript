fbbc29080428d3667a8032cc1ee2f0fff3e14408
Kristiana Blake <kristiana.blake@gmail.com>
2020-02-17 12:56:08 -0600

    express is working

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, '/js/app.js')));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+	res.send()
+})
+
+
+
+app.listen(PORT, () => console.log(`Server started on on port ${PORT}`));
